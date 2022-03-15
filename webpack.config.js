221835794977c73f2491a4a5cbd14d559e5012ec
Soohyun Lim <d2axasap@gmail.com>
2022-03-14 23:15:07 -0400

    Create webpack.config.js for bundling JavaScript files. Use babel-loader to transpile JSX in React. Move babel presets into webpack and delete .babelrc. Rename App.js to App.jsx. Use live server on index.html to visualize changes in the browser.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  mode: 'development'
+};
+
+module.exports = config;
