551c3103d9ffe3cd7ebce348a65a80a880939e33
Soohyun Lim <d2axasap@gmail.com>
2022-03-17 23:26:30 -0400

    Create device.js and store mobile and desktop min-width information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Axios from 'axios';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
 import TopBar from './components/TopBar';
 import SidebarNavigation from './components/SidebarNavigation';
