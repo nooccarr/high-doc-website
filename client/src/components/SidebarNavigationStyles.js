32156c6493dad947f72055458fe7103a74b5551b
Soohyun Lim <d2axasap@gmail.com>
2022-03-29 23:49:13 -0400

    Remove double scroll bars in the mobile version

--- client/src/components/SidebarNavigationStyles.js
+++ client/src/components/SidebarNavigationStyles.js
@@ -4,12 +4,13 @@ export const SidebarNavigationWrap = styled.nav`
   .nav-container {
     margin-right: 16px;
     position: sticky;
-    height: 100vh;
+
     overflow: auto;
     padding-top: 8px;
 
     @media (min-width: 800px) {
       display: grid;
+      height: 100vh;
     }
   }
 
